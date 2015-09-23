2d2a8a9df782a33b2e0bc153067dc00d5910d543
user <user@Atmiksoni>
2015-09-24 00:31:55 +0530

    sddf

--- SimpleSpringMVC/src/main/java/com/ams/mvc/HomeController.java
+++ SimpleSpringMVC/src/main/java/com/ams/mvc/HomeController.java
@@ -24,7 +24,7 @@ public class HomeController {
 	 */
 	 @RequestMapping(value = {"/", "/home"}, method = RequestMethod.GET)
 	    public String home(Locale locale, Model model) {
-	        logger.info("Welcome home! The client locale is  dfd {}.", locale);
+	        logger.info("Welcome home! Thejj client locale is  dfd {}.", locale);
 	         
 	        Date date = new Date();
 	        DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
